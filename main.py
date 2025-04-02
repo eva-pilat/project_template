a9aee62361e6f8cc022b63749812d527f0b1bbef
Eva Pilat <evissdun@icloud.com>
2025-04-02 19:11:32 +0300

    Output fixes

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ import app.io.input as app_input
 import app.io.output as app_output
 
 if __name__ == "__main__":
-    file_in = "/Users/evadun/PycharmProjects/project_template/data"
+    file_in_txt = "/Users/evadun/PycharmProjects/project_template/data/input.txt"
+    file_in_csv = "/Users/evadun/PycharmProjects/project_template/data/input.csv"
+    file_copy_csv = "/Users/evadun/PycharmProjects/project_template/data/input_copy.csv"
     # data = "{1: 'one', 2: 'two', 3: 'three'}"
     # call our input func and then storing input in the data file
     text_input = app_input.input_from_console()
-    app_output.write_file_python(file_in, text_input)
+    app_output.write_file_python(file_in_txt, text_input)
 
-    file_content = app_input.read_file_python(file_in)
-    app_output.write_file_python(file_in, file_content)
+    file_content = app_input.read_file_python(file_in_csv)
+    app_output.write_file_python(file_copy_csv, file_content)
 
-    file_content_pandas = app_input.read_file_pandas(file_in)
-    app_output.write_file_python(file_in, str(file_content_pandas))
+    file_content_pandas = app_input.read_file_pandas(file_in_csv)
+    app_output.write_file_python(file_in_csv, str(file_content_pandas))
